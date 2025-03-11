5946ce52816153b27134a166fe9bc986492dbde9
madiazv1 <diazvalenciam9@gmail.com>
2025-03-11 15:39:50 -0500

    Nuevo modelo de profesiones para trabajar

--- professions/models.py
+++ professions/models.py
@@ -4,6 +4,8 @@ class Profession(models.Model):
     profession_name = models.CharField(max_length=100, unique=True)
     current_salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     future_salaries = models.JSONField(default=list)
+    companies = models.JSONField(default=list)
+    experience = models.CharField(max_length=3, default="JR")
 
     def __str__(self):
         return self.profession_name
