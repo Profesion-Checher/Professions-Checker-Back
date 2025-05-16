3ea22e20f2481623afe2efbd2aaeb963e2b18201
madiazv1 <diazvalenciam9@gmail.com>
2025-05-16 16:21:27 -0500

    Modificacion para jwt

--- profesionCheckerBackEnd/settings.py
+++ profesionCheckerBackEnd/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
-from date import timedelta
+from datetime import timedelta
 import os
 
 load_dotenv()
--- users/models.py
+++ users/models.py
@@ -4,12 +4,21 @@ from django.db import models
 class ClientManager(BaseUserManager):
   def create_user(self, email, first_name, last_name, password=None, **extra_fields):
     if not email:
-      raise ValueError('El correo es obligatorio')
+        raise ValueError('El correo es obligatorio')
     email = self.normalize_email(email)
-    user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+
+    user = self.model(
+        email=email,
+        first_name=first_name,
+        last_name=last_name,
+        company=extra_fields.get('company'),
+        profession=extra_fields.get('profession'),
+        salary=extra_fields.get('salary'),
+    )
     user.set_password(password)
     user.save(using=self._db)
     return user
+
 
   def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
     extra_fields.setdefault('is_staff', True)
