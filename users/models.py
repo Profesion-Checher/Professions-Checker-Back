ebf4522ad3c44d230a10ab1e7b35b6c14569f9f2
madiazv1 <diazvalenciam9@gmail.com>
2025-05-16 16:21:55 -0500

    Modificacion para jwt

--- users/models.py
+++ users/models.py
@@ -19,7 +19,6 @@ class ClientManager(BaseUserManager):
     user.save(using=self._db)
     return user
 
-
   def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
     extra_fields.setdefault('is_staff', True)
     extra_fields.setdefault('is_superuser', True)
