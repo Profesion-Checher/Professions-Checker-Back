3bc20522fd5b20e990f135f1af8d497f9197e757
Jvelasquez980 <jeronimo.velasquez.escobar@gmail.com>
2025-05-16 22:51:57 -0500

    Implement data loading functionality and update salary prediction API

--- aiModel/urls.py
+++ aiModel/urls.py
@@ -3,4 +3,5 @@ from .views import SalaryPredictionView
 
 urlpatterns = [
     path("predict-salary/", SalaryPredictionView.as_view(), name="predict_salary"),
+    path("load_data/", SalaryPredictionView.as_view(), name="load_data"),
 ]
